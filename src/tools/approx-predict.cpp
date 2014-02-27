47970c638ea60c23fe98c060f53ba1d2455e1e90
Marc Claesen <claesenm@gmail.com>
2014-02-27 20:50:31 +0100

    Update approx-predict.cpp

--- src/tools/approx-predict.cpp
+++ src/tools/approx-predict.cpp
@@ -1,20 +1,7 @@
 /**
- *  Copyright (C) 2013 KU Leuven
+ *  Copyright (C) 2014 Marc Claesen
  *
  *  This file is part of ApproxSVM.
- *
- *  ApproxSVM is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published
- *  by the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  ApproxSVM is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with ApproxSVM.  If not, see <http://www.gnu.org/licenses/>.
  *
  * approx-predict.cpp
  *
